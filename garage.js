929097acc1950ea5606754f1d3b476448a1a8136
aaron <aaronkhabra@yahoo.co.uk>
2017-08-24 14:22:45 +0100

    basic functionality for garage done

--- garage.js
+++ garage.js
@@ -1,3 +1,38 @@
 "use strict";
 
 
+//console.log(document.getElementById("tyreCheck").value);
+//console.log(document.getElementById("tyreCheck").checked);
+
+//variable declaration
+let carArray = [];
+
+
+function makeCar(){
+	let car = {make: document.getElementById("make").value, reg:document.getElementById("reg").value, cost:0};
+	
+	if(document.getElementById("tyreCheck").checked){
+		car.cost = Number(car.cost) + Number(document.getElementById("tyreCheck").value);
+	}
+	if(document.getElementById("windscreen").checked){
+		car.cost = Number(car.cost) + Number(document.getElementById("windscreen").value);
+	}
+	if(document.getElementById("respray").checked){
+		car.cost = Number(car.cost) + Number(document.getElementById("respray").value);
+	}
+	console.log(car.make, car.reg, car.cost);
+	carArray.push(car);
+}
+
+function removeCar(){
+	carArray.splice(-1, 1);
+	console.log("Car Removed");
+}
+
+function printCarArray(){
+	for(let i =0; i< carArray.length; i++){
+		console.log(carArray[i].make, carArray[i].reg, carArray[i].cost);
+	}
+	
+
+}
